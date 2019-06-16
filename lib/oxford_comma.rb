5d236618cc8cc8530a94714df83c91e8f6733595
Yosef Lehrer <yolehrer@gmail.com>
2019-06-16 06:11:44 +0000

    Automatically backed up by Learn

---
+++ lib/oxford_comma.rb
@@ -0,0 +1,8 @@
+def oxford_comma(array)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert( "and ")
+  end
+  array.join(", ")    
+end
